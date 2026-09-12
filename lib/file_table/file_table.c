79dd3a9dbe19cefd7abacb05ce918e6ea1cd2140
kshakirov <kirill.shakirov4@gmai.com>
2026-09-12 19:31:17 +0300

    wip: sketch file table metadata arrays

--- lib/file_table/file_table.c
+++ lib/file_table/file_table.c
@@ -1,7 +1,23 @@
 #include "file_table.h"
 #include "../../debug.h"
 
+
+#define CAPACITY 10000
 typedef uint32_t FileIndex ;
+
+typedef uint32_t FileNameHash ;
+
+typedef  struct {
+  const char* filename;
+  const size_t size;
+  const FileIndex descr;
+  const FileNameHash fileNameHash;
+  //dont' now how make Block address available her
+}FileMetadata;
+
+static FileNameHash fileNameHashTable[CAPACITY];
+
+static FileMetadata fileMetadataTable[CAPACITY];
 
 FileIndex put_file(const char* content, const char* filename){
   DEBUG_LOG("Saving the file");
